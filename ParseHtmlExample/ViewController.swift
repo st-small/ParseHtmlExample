9ca13d84ea1a34a2ae13dbafe8f01bcaf084ab8c
Станислав Шияновский <st-small@rambler.ru>
2019-04-12 13:29:43 +0300

    Initial Commit

---
+++ ParseHtmlExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParseHtmlExample
+//
+//  Created by Stanly Shiyanovskiy on 4/12/19.
+//  Copyright © 2019 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
